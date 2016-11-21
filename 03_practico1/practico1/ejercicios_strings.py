a758ad981405cbed2760aecb5627507d72ec2361
Rodrigo Crespillo <rodixxi@gmail.com>
2016-11-21 17:52:53 -0300

    ejercicios_string

---
+++ 03_practico1/practico1/ejercicios_strings.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+def facturas(nro):
+
+    cantidad = nro
+    if cantidad >= 12:
+        cantidad = "muchas"
+    return "Cantidad de facturas: " + str(cantidad)
+
+def ambos(s):
+    cadena = s
+    if len(s) < 2:
+        cadena = ""
+    else:
+        cadena = s[:2] + s[-2:]
+    return cadena
+
+def fix(s):
+    capital = s[:1]
+    cadena = s[1:]
+    return capital + cadena.replace(capital, "*", 1)
+
+def mezclar(a, b):
+    separador = " "
+    cadena1 = b[:1] + a[1:]
+    cadena2 = a[:1] + b[1:]
+    return separador.join((cadena1, cadena2))
