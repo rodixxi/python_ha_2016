0116649d79fb20eab276da221c22f5f7e1693490
Rodrigo Crespillo <rodixxi@gmail.com>
2016-11-22 14:57:11 -0300

    algunos cambios de PEP8

--- 03_practico1/practico1/ejercicios_strings.py
+++ 03_practico1/practico1/ejercicios_strings.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 
-def facturas(nro):
 
+def facturas(nro):
     cantidad = nro
     if cantidad >= 12:
         cantidad = "muchas"
     return "Cantidad de facturas: " + str(cantidad)
+
 
 def ambos(s):
     cadena = s
@@ -15,13 +16,16 @@ def ambos(s):
         cadena = s[:2] + s[-2:]
     return cadena
 
+
 def fix(s):
     capital = s[:1]
     cadena = s[1:]
     return capital + cadena.replace(capital, "*", 1)
+
 
 def mezclar(a, b):
     separador = " "
     cadena1 = b[:1] + a[1:]
     cadena2 = a[:1] + b[1:]
     return separador.join((cadena1, cadena2))
+
