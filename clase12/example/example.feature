25f00a02ada987873a98edcdf14fb8b79017f7c6
Juan B Cabral <jbc.develop@gmail.com>
2016-12-14 19:46:45 -0300

    example

--- clase12/example/example.feature
+++ clase12/example/example.feature
@@ -1,9 +1,20 @@
 Feature: showing off behave
 
     Scenario: run a simple test of suma
-        Given the number "1" and the number "2",
+        Given the number "1" as "int" and the number "2" as "int",
             when apply the function "suma"
-            then i expect the result is "3"
+            then i expect the result is "3" as "int"
+
+    Scenario: run a simple test of division
+        Given the number "1" as "int" and the number "2" as "float",
+            when apply the function "division"
+            then i expect the result is "0.5" as "float"
+
+    Scenario: run a simple test of division by 0
+        Given the number "1" as "int" and the number "0" as "float",
+            and i expect an error,
+            when apply the function "division"
+            then i expect an error of type "ZeroDivisionError"
 
 
 
--- clase12/example/steps/example.py
+++ clase12/example/steps/example.py
@@ -3,20 +3,46 @@ from behave import *
 
 import operators as op
 
+TYPES = {t.__name__: t for t in (int, float, complex)}
 
-@given('the number "{number}" and the number "{other_number}",')
-def step_impl(context, number, other_number):
-    context.number = int(number)
-    context.other_number = int(other_number)
+
+@given('the number "{number}" as "{typen}" and the number "{other_number}" as "{typeon}",')
+def step_impl(context, number, typen, other_number, typeon):
+    typen = TYPES[typen]
+    typeon = TYPES[typeon]
+    context.number = typen(number)
+    context.other_number = typeon(other_number)
+
+@given(u'i expect an error,')
+def step_impl(context):
+    def catch(func):
+        def inner(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except Exception as err:
+                context.error = err
+        return inner
+    context.catch = catch
+
 
 
 @when('apply the function "{function}"')
 def step_impl(context, function):
     function = getattr(op, function)
+    if hasattr(context, "catch"):
+        function = context.catch(function)
     context.function_result = function(context.number, context.other_number)
 
 
-@then('i expect the result is "{result}"')
-def step_impl(context, result):
-    result = int(result)
+@then('i expect the result is "{result}" as "{typer}"')
+def step_impl(context, result, typer):
+    typer = TYPES[typer]
+    result = typer(result)
     assert context.function_result == result, "{} != {}".format(context.function_result, result)
+
+
+@then(u'i expect an error of type "{error_name}"')
+def step_impl(context, error_name):
+    actual = type(context.error).__name__
+    assert actual == error_name, "{} != {}".format(actual, error_name)
+
