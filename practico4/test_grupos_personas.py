3756399d96d3b45b66e0f52770ee6c93df035efc
Rodrigo Crespillo <rodixxi@gmail.com>
2016-12-22 10:43:53 -0300

    addedd def for each feature

--- practico4/test_grupos_personas.py
+++ practico4/test_grupos_personas.py
@@ -3,18 +3,21 @@
 
 import unittest
 
-from grupos_personas import *
+from grupos_personas import Person, Group
 
 
 class Groups(unittest.TestCase):
-    p0 = Person("juan", 18)
-    p1 = Person("tito", 34)
-    p2 = Person("carlos", 36)
-    p3 = Person("jose", 28)
 
-    def test_groups(self):
+    def setUp(self):
+        self.p0 = Person("juan", 18)
+        self.p1 = Person("tito", 34)
+        self.p2 = Person("carlos", 36)
+        self.p3 = Person("jose", 28)
 
-        # Two Persons class can get together and generate a Group
+    def test_generate_by_add(self):
+        """
+        Two Persons class can get together and generate a Group
+        """
 
         g0 = self.p0 + self.p1
 
@@ -23,15 +26,22 @@ class Groups(unittest.TestCase):
         self.assertIn(self.p1, g0)
         self.assertEqual(len(g0), 2)
 
-        #  Two Groups are equal if they have the same Persons
-        self.assertEqual(Group({self.p0, self.p1}), Group([self.p1, self.p0]))
+    def test_eq(self):
+        """
+        Two Groups are equal if they have the same Persons
+        """
 
+        self.assertEqual(Group({self.p0, self.p1}), Group([self.p1, self.p0]))
         self.assertNotEqual(Group({self.p0, self.p1}), Group([self.p2,
                                                               self.p0]))
 
-        # A Group can add another Person and generate a new Group with all
-        # the persons of the Group plus the new Person
+    def test_add_person(self):
+        """
+        A Group can add another Person and generate a new Group with all
+        the persons of the Group plus the new Person
+        """
 
+        g0 = self.p0 + self.p1
         g1 = g0 + self.p2
 
         self.assertIsInstance(g1, Group)
@@ -40,8 +50,13 @@ class Groups(unittest.TestCase):
         self.assertIn(self.p2, g1)
         self.assertEqual(len(g1), 3)
 
-        # You can subtract one person from a group
+    def test_sub_person(self):
+        """
+        You can subtract one person from a group
+        """
 
+        g0 = self.p0 + self.p1
+        g1 = g0 + self.p2
         g2 = g1 - self.p2
 
         self.assertIsInstance(g2, Group)
@@ -50,11 +65,16 @@ class Groups(unittest.TestCase):
         self.assertNotIn(self.p2, g2)
         self.assertEqual(len(g2), 2)
 
-        # Two groups can be added and subtracted. In the case of addition the
-        # sum generates a group with the persons of the TWO groups without
-        # repeating; And in the case of subtract a group is created where only
-        # the people of the first group where they are not in the second group.
+    def test_add_group(self):
+        """
+        Two groups can be added and subtracted. In the case of addition the
+        sum generates a group with the persons of the TWO groups without
+        repeating; And in the case of subtract a group is created where only
+        the people of the first group where they are not in the second group.
+        """
 
+        g0 = self.p0 + self.p1
+        g1 = g0 + self.p2
         g2 = self.p1 + self.p2
         g3 = g1 + g2
 
@@ -64,6 +84,11 @@ class Groups(unittest.TestCase):
         self.assertIn(self.p2, g3)
         self.assertEqual(len(g3), 3)
 
+    def test_sub_group(self):
+
+        g0 = self.p0 + self.p1
+        g1 = g0 + self.p2
+        g2 = self.p1 + self.p2
         g3 = g1 - g2
 
         self.assertIsInstance(g3, Group)
@@ -72,35 +97,56 @@ class Groups(unittest.TestCase):
         self.assertNotIn(self.p2, g3)
         self.assertEqual(len(g3), 1)
 
-        # Groups are iterative and iterate over persons.
+    def test_iter(self):
+        """
+        Groups are iterative and iterate over persons.
+        """
 
-        self.assertIn(self.p0, g3)
-        self.assertNotIn(self.p1, g3)
+        g0 = self.p0 + self.p1
 
-        # The groups have length (number of persons)
+        self.assertIn(self.p0, g0)
+        self.assertNotIn(self.p2, g0)
 
-        g3 = g3 - self.p0
+    def test_len(self):
+        """
+        The groups have length (number of persons)
+        """
+        g0 = self.p0 + self.p1
+        g1 = g0 - self.p0
 
-        self.assertEqual(len(g3), 0)
-        self.assertEqual(len(g2), 2)
+        self.assertEqual(len(g1), 1)
+        self.assertEqual(len(g0), 2)
 
-        # A group is True only if it has any person.
+    def test_bool(self):
+        """
+        A group is True only if it has any person.
+        """
 
-        self.assertTrue(g2)
-        self.assertFalse(g3)
+        g0 = self.p0 + self.p1
+        g1 = self.p0 + self.p1
+        g2 = g0 - g1
 
-        # You can tell if a person is in a group with the operator "in"
+        self.assertTrue(g0)
+        self.assertFalse(g2)
 
-        self.assertTrue(self.p1 in g2)
-        self.assertFalse(self.p1 in g3)
+    def test_in(self):
+        """
+        You can tell if a person is in a group with the operator "in"
+        """
+        g0 = self.p0 + self.p1
+        self.assertTrue(self.p1 in g0)
+        self.assertFalse(self.p2 in g0)
 
-        # NOT IMPLEMENTED
-        # The group has an edad_promedio method that calculates the average
-        # age of the group (float) or throws an EmptyGroupError if the group
-        # is empty
-
+    def test_edad_promedio(self):  # NOT IMPLEMENTED
+        """
+        The group has an edad_promedio method that calculates the average
+        age of the group (float) or throws an EmptyGroupError if the group
+        is empty
+        """
         # self.assertIsInstance(g2.edad_promedio(), float)
         # self.assertRaises(EmptyGroupError, g3.edad_promedio)
+        pass
+
 
 if __name__ == '__main__':
     unittest.main()
